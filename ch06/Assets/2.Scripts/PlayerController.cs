c4ff7126ef9cb17eb82d7020a94118e1ea6fdbc0
0sun <dladudtjs9@gmail.com>
2025-04-28 10:53:29 +0900

    ch6구름복사까지

--- ch06/Assets/2.Scripts/PlayerController.cs
+++ ch06/Assets/2.Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
+    Animator animator;
     float jumpForce = 370f;
     float walkForce = 30f;
     float maxWalkSpeed = 2f;
@@ -13,12 +14,14 @@ public class PlayerController : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid2D.AddForce(transform.up * jumpForce);
             //rigid2D.AddForce(new Vector2(0,1) * jumpForce));
@@ -39,5 +42,6 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(key, 1, 1);
         }
+        animator.speed = speedx / 2.0f;
     }
 }
