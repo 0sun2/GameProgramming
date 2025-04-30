2c7d61b3a597fc06226c30811d98f68d0291b033
0sun <dladudtjs9@gmail.com>
2025-04-30 15:47:15 +0900

    ch6종료씬

--- ch06/Assets/2.Scripts/PlayerController.cs
+++ ch06/Assets/2.Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float jumpForce = 370f;
     float walkForce = 30f;
     float maxWalkSpeed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +22,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
-            rigid2D.AddForce(transform.up * jumpForce);
-            //rigid2D.AddForce(new Vector2(0,1) * jumpForce));
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
         int key = 0;
-        if(Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
-        
+
         float speedx = Mathf.Abs(rigid2D.velocity.x);
 
         if (speedx < maxWalkSpeed)
@@ -38,10 +38,21 @@ public class PlayerController : MonoBehaviour
             rigid2D.AddForce(transform.right * key * walkForce);
         }
 
-        if(key !=0)
+        if (key != 0)
         {
             transform.localScale = new Vector3(key, 1, 1);
         }
         animator.speed = speedx / 2.0f;
+      
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Goal");
+        SceneManager.LoadScene("ClearScene");
     }
 }
